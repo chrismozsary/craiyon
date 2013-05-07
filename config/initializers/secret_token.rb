23a1d68c2955bd9ee303cc3e2de7484b1a6b8236
chrismozsary <christopher@mozsary.com>
2013-05-07 18:50:27 -0400

    This is my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craiyon::Application.config.secret_token = '0df128e547a5bfae5f72ae1060e6de963f9a858025620164d094e7d27105792382c177e48501e5790c2f65ed89c112cd8244a14b980862c07062d83348625fba'
